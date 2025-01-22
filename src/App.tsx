faf6287017c6f97d5d7732d1d147c7fa0d779661
Brendan Lee <brendanjlee5@gmail.com>
2025-01-22 14:06:25 -0800

    chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Provider } from "./components/ui/provider";
+
 function App() {
   return (
-    <>
+    <Provider>
       <h1>Welcome to my portfolio</h1>
-    </>
-  )
+    </Provider>
+  );
 }
 
-export default App
+export default App;
